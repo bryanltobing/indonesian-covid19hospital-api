e03668154258c84795fb338c1cf445bfabbaa577
bryantobing12 <bryantobing0@gmail.com>
2020-10-03 20:32:19 +0700

    add database connection

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const publicDirectoryPath = path.join(__dirname, './public');
+// db connection
+require('./db/mongoose');
+
+const publicDirectoryPath = path.join(__dirname, './../public');
 
 app.use(express.static(publicDirectoryPath));
 app.set('view engine', 'ejs');
